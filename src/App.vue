2f3289aefce248bb4043631140687a14005937a2
current <dzariboo@ukr.net>
2022-04-29 16:42:30 +0300

    вынести пагинацию в отдельный компонент

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,132 @@
 <template>
     <div class="app">
-        <post-form @create="createPost"/>
-        <post-list v-bind:posts="posts"
-                   v-on:remove="removePost"/>
+        <h1>Страница с постами</h1>
+        <my-input
+                v-model="searchQuery"
+                placeholder="Поиск..."
+        />
+        <div class="app__btns">
+            <my-button class="btn" v-on:click="showDialog">Create</my-button>
+            <my-select
+                    v-bind:options="sortOptions"
+                    v-model="selectedSort"/>
+        </div>
+        <my-dialog v-model:show="dialogVisible">
+            <post-form @create="createPost"/>
+
+        </my-dialog>
+        <post-list v-bind:posts="sortedAndSearchedPosts"
+                   v-on:remove="removePost"
+                   v-if="!isPostsLoading"
+        />
+        <div v-else>Идет загрузка...</div>
+        <div class="page__wrapper">
+        <div v-for="pageNumber in totalPages"
+             :key="pageNumber"
+             class="page"
+             v-bind:class="{
+             'current-page': page === pageNumber
+             }"
+        >{{pageNumber}}</div>
+        </div>
     </div>
 </template>
 
 <script>
     import PostForm from "./components/PostForm";
     import PostList from "./components/PostList";
+    import axios from "axios";
+
     export default {
-        components:{
+        components: {
             PostList, PostForm
         },
         data() {
-            return{
-                posts:[
-                    {id: 1, title:"Name1", body:"Body1"},
-                    {id: 2, title:"Name2", body:"Body2"},
-                    {id: 3, title:"Name3", body:"Body3"},
+            return {
+                posts: [],
+                dialogVisible: false,
+                modificatorValue: '',
+                isPostsLoading: false,
+                selectedSort: '',
+                searchQuery: '',
+                page: 1,
+                limit: 10,
+                totalPages: 0,
+                sortOptions: [
+                    {value: 'title', name: 'По названию'},
+                    {value: 'body', name: 'По содержимому'},
                 ],
             }
         },
         methods: {
-            createPost(post){
+            createPost(post) {
                 this.posts.push(post);
+                this.dialogVisible = false;
             },
-            removePost(post){
-                this.posts = this.posts.filter(p=>p.id!==post.id)
+            removePost(post) {
+                this.posts = this.posts.filter(p => p.id !== post.id)
+            },
+            showDialog() {
+                this.dialogVisible = true;
+            },
+            async fetchPosts() {
+                try {
+                    this.isPostsLoading = true;
+                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                        params:{
+                            _page: this.page,
+                            _limit: this.limit
+                        }
+                    });
+                    this.totalPages = Math.ceil(response.headers['x-total-count']/this.limit);
+                    this.posts = response.data;
+                } catch (e) {
+                    alert('ошибка')
+                } finally {
+                    this.isPostsLoading = false;
+                }
+            },
+        },
+        mounted() {
+            this.fetchPosts()
+        },
+        computed:{
+            sortedPosts(){
+                return [...this.posts].sort((post1, post2)=>{
+                    return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+                })
+            },
+            sortedAndSearchedPosts(){
+                return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
             }
-        }
+        },
     }
 </script>
 
 <style>
-*{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-.app{
-    padding:20px;
-}
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 
+    .app {
+        padding: 20px;
+    }
+
+    .app__btns {
+        display: flex;
+        justify-content: space-between;
+    }
+    .page__wrapper{
+        display: flex;
+        margin-top: 15px;
+    }
+    .page{
+        border: 1px solid black;
+        padding: 10px;
+    }
+    .current-page{
+        border: 3px solid teal;
+    }
 </style>
---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,32 @@
+<template>
+    <select v-model="modelValue" v-on:change="changeOption">
+        <option disabled value="">Выберите из списка</option>
+        <option v-for="option in options"
+                v-bind:key="option.value"
+                v-bind:value="option.value">{{option.name}}</option>
+    </select>
+</template>
+
+<script>
+    export default {
+        name: "my-select",
+        props:{
+            modelValue: {
+                type: String,
+            },
+            options:{
+                type: Array,
+                default: ()=>[],
+            },
+        },
+        methods:{
+            changeOption(event){
+                this.$emit('update:modelValue', event.target.value)
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
